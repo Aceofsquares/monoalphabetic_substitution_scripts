1dc25c54fde87dc9e12e484c8f352a37ae247d7e
Brian Rogers <btcr1990@gmail.com>
2018-09-17 22:27:19 -0400

    Add help message: genkey.py

--- genkey.py
+++ genkey.py
@@ -2,6 +2,7 @@
 
 from string import ascii_uppercase as upper
 from random import sample
+from sys import argv, exit
 
 '''
 Quickly generates a monoalphabetic key used
@@ -17,4 +18,9 @@ Y W U P F N M C B Q O T E D R G A S L Z J V I X H K
 where A maps to Y, B maps to W, etc.
 '''
 
+if len(argv) > 1:
+    for arg in argv:
+        if arg in ['-h', '--help']:
+            print 'Generates a random 26 character key for monoalphabetic substitution ciphers'
+            exit(0)
 print ''.join(sample(upper, len(upper)))
